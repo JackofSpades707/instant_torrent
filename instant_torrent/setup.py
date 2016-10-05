650d28051ec13708013ca805adf2e6afb560321d
jackofspades707 <jackofspades707@hotmail.com>
2016-10-04 19:21:56 -0700

    some setup config

---
+++ instant_torrent/setup.py
@@ -0,0 +1,16 @@
+
+m setuptools import setup
+from sys import platform
+
+setup(name='instanttorrent',
+            version='1.0',
+            description='Instantly download any torrent!',
+            url='https://github.com/jackofspades707/instant_torrent',
+            author='JackofSpades707',
+            author_email='JackofSpades707@gmail.com',
+            license='MIT',
+            packages=['instanttorrent'],
+            scripts=['bin/instanttorrent'],
+            install_requires=['requests',
+                              'bs4']),
+            zip_safe=False)
